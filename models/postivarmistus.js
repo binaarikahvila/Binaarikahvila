e5604c0de72e01bead77246372b99fde42b2f1d5
Roope O <binaarikahvila@gmail.com>
2017-04-28 15:03:49 +0300

    UTF-8

--- models/postivarmistus.js
+++ models/postivarmistus.js
@@ -15,7 +15,7 @@ exports.lahetaPosti = function(osoite){
 		from: 'binaarikahvila@gmail.com',
 		to: osoite,
 		subject: 'Varauksesi Bin‰‰rikahvilaan on vastaanotettu',
-		html: '<h1>Varauksesi</h1>\n<p>Kiitokset ajanvarauksesta Bin‰‰rikahvilan tiloissa! ' +
+		html: '<h1>Varauksesi on vastaanotettu</h1>\n<p>Kiitokset ajanvarauksesta Kahvila Bin‰‰rin tiloissa! ' +
 			'Pyrimme tarkistamaan antamanne tiedot mahdollisimman nopeasti ja vahvistamaan ' +
 			'tilauksenne. <b>Odotamme innolla tuloanne!</b>',
 		generateTextFromHtml: true,		
