9a5b994116643cfc3e4e5bf28e03e579f32a8d25
Roope O <binaarikahvila@gmail.com>
2017-04-30 13:41:42 +0300

    Sähköpostivarmistus

--- routes/tilaus.js
+++ routes/tilaus.js
@@ -19,33 +19,37 @@ router.post('/', function(req, res, next) {
 	if(dd<10){dd='0'+dd} if(mm<10){mm='0'+mm}
     today = yyyy+"-"+mm+"-"+dd;
 	
-	postiLahetys.lahetaPosti(req.body);
+	//Tarkistaa, että sähköpostilähetys onnistui ja sitten vasta päivittää tietokannan
+	if (postiLahetys.lahetaPosti(req.body)) {
 	
 	/*Tietokannan päivitys - mikäli samalla sähköpostilla, tapahtuman kuvauksella sekä
 	tilauspäivämäärällä ja -ajalla on jo luotu tilaus, muokkaa sen tietoja.
 	*/
-	tilausKaavake.update(
-		{ sposti: req.body.sahkoposti,				
-		tapahtuma: req.body.tapahtuma,
-		tilausPaiva: req.body.paiva,
-		tilausTunti: req.body.alku,
-		},
-		{ pvm: today,
-		nimi: req.body.nimi,
-		puhelin: req.body.puhnro,
-		maxOsallistuja: req.body.osallistujat,
-		onkoTarjoilu: req.body.tarjoilu,		
-		tilausKesto: req.body.kesto,
-		kommentti: req.body.kommentti },
-		{ upsert: true },		
-		function(err) {
-			if(err) {
-				console.error(err.stack);
-				return res.redirect(303, '/error');
-			}
-			return res.redirect(303, '/tilaus');
-		}		
-	);	
+		tilausKaavake.update(
+			{ sposti: req.body.sahkoposti,				
+			tapahtuma: req.body.tapahtuma,
+			tilausPaiva: req.body.paiva,
+			tilausTunti: req.body.alku,
+			},
+			{ pvm: today,
+			nimi: req.body.nimi,
+			puhelin: req.body.puhnro,
+			maxOsallistuja: req.body.osallistujat,
+			onkoTarjoilu: req.body.tarjoilu,		
+			tilausKesto: req.body.kesto,
+			kommentti: req.body.kommentti },
+			{ upsert: true },		
+			function(err) {
+				if(err) {
+					console.error(err.stack);
+					return res.redirect(303, '/error');
+				}
+				return res.redirect(303, '/vahvistus');
+			}		
+		);
+	} else {
+		return res.redirect(303, '/error');
+	}
 });
 
 module.exports = router;
